be4f5c8fcb23a32b88c4aa6950afa97cb46ff746
Josef Chen <josef@velonto.at>
2019-11-25 16:38:03 +0100

    Changed 404 message

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -5,7 +5,7 @@ export default () => {
     return (
         <div class='boxed-view'>
             <div class="boxed-view__box">
-                            <h2>PAGE NOT FOUND</h2>
+                            <h2>404 PAGE NOT FOUND</h2>
                                 <p>Hmmmm, we cannot find the page you're looking for.</p>
                                 <Link  to="/" className="button button--link">Go back to home</Link>
             </div>
